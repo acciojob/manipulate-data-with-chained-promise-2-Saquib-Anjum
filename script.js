9f5a819b6a5df5981baa1680765c80b951f37cc5
Saquib Anjum <134352662+Saquib-Anjum@users.noreply.github.com>
2026-09-02 23:14:02 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,37 @@
-//your JS code here. If required.
+const output = document.querySelector("#output");
+
+let arr = [1, 2, 3, 4];
+
+const p1 = new Promise((resolve, reject) => {
+    setTimeout(() => {
+        resolve(arr);
+    }, 1000);
+});
+
+p1.then((data) => {
+
+    let res = data.filter((ele) => ele % 2 === 0);
+    let str = res.join(",");
+
+    const ptag = document.createElement("p");
+    ptag.innerText = str;
+    output.append(ptag);
+
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve(res);
+        }, 2000);
+    });
+
+}).then((data) => {
+
+    let res = data.map((ele) => ele * 2);
+    let str = res.join(",");
+
+    const ptag = document.createElement("p");
+    ptag.innerText = str;
+    output.append(ptag);
+
+}).catch((err) => {
+    console.log(err);
+});
